a278211cdb2886a54d094fb0e996ddc587f59f88
thinkAmi <dev.thinkami+git@gmail.com>
2016-07-09 07:51:31 +0900

    add send_gmail_by_smtp

---
+++ send_gmail_by_smtp/send_gmail_by_cmdlet.ps1
@@ -0,0 +1,21 @@
+﻿$mail = @{
+    from = "example@gmail.com";
+    to = "example+to@gmail.com";
+    smtp_server = "smtp.gmail.com";
+    smtp_port = 587;
+    user = "example@gmail.com";
+    password = "1234";
+}
+
+$password = ConvertTo-SecureString $mail["password"] -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential $mail["user"], $password
+
+Send-MailMessage -To $mail["to"] `
+                 -From $mail["from"] `
+                 -SmtpServer $mail["smtp_server"] `
+                 -Credential $credential `
+                 -Port $mail["smtp_port"] `
+                 -Subject "subject" `
+                 -Body "body: Hello Cmdlet" `
+                 -Encoding UTF8 `
+                 -UseSsl
---
+++ send_gmail_by_smtp/send_gmail_by_net_class.ps1
@@ -0,0 +1,20 @@
+﻿$mail = @{
+    from = "example@gmail.com";
+    to = "example+to@gmail.com";
+    smtp_server = "smtp.gmail.com";
+    smtp_port = 587;
+    user = "example@gmail.com";
+    password = "1234";
+}
+
+$client = New-Object Net.Mail.SmtpClient($mail["smtp_server"], $mail["smtp_port"])
+
+# GmailはSMTP + SSLで送信する
+$client.EnableSsl = $true
+
+# SMTP Authのため、認証情報を設定する
+$client.Credentials = New-Object Net.NetworkCredential($mail["user"], $mail["password"])
+
+$msg = New-Object Net.Mail.MailMessage($mail["from"], $mail["to"], "subject", "body: Hello Net class")
+
+$client.Send($msg)
