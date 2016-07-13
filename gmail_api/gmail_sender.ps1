fcb874bd10b7da56294d8593d28debb2ed50662b
thinkAmi <dev.thinkami+git@gmail.com>
2016-07-13 23:15:55 +0900

    add Gmail API sample (gmail_sender)

---
+++ gmail_api/gmail_sender.ps1
@@ -0,0 +1,98 @@
+﻿# 参考 https://www.administrator.de/wissen/powershell-googlemail-gmail-nativ-powershell-verwalten-291531.html
+
+function ConvertTo-Base64Url($str){
+    # 参考 http://jason.pettys.name/2014/10/27/sending-email-with-the-gmail-api-in-net-c/
+
+    # ToBase64String()で使うため、バイト列にエンコード
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($str)
+    # バイトをBase64でデコードした文字列にする
+    $b64str = [System.Convert]::ToBase64String($bytes)
+    # ToBase64String()ではURLセーフなBase64ではないため、不足している部分を自分で置き換え
+    $without_plus = $b64str -replace '\+', '-'
+    $without_slash = $without_plus -replace '/', '_'
+    $without_equal = $without_slash -replace '=', ''
+
+    return $without_equal
+}
+
+function Send-Gmail(){
+    # メールの送受信情報
+    $mail = @{
+        from = "example@gmail.com";
+        to = "example+to@gmail.com";
+    }
+
+    # 別モジュールからGoogleの認証情報を取得
+    # https://msdn.microsoft.com/en-us/library/dd878284.aspx
+    $path = Join-Path . "google_credential.psm1"
+    Import-Module -Name $path
+    $credential = Get-GoogleCredential
+
+    # 念のため、認証情報を表示
+    Write-Host $credential
+
+    if (-not $credential){
+        # 認証情報がない場合は、処理しない
+        Write-Host "Not Authenticated."
+        return
+    }
+
+    # 外部ライブラリ`AE.NET.Mail`のdllを読み込んで、使用する
+    # 事前準備として、AE.NET.Mailライブラリは、.NETのバージョンが一致するものを選び、ブロックの解除を行う
+    # ブロックの解除 http://stackoverflow.com/questions/18801440/powershell-load-dll-got-error-add-type-could-not-load-file-or-assembly-webdr
+    # PS S:\Sandbox\gmai_api> $PSVersionTable
+    # Name                           Value
+    # ----                           -----
+    # PSVersion                      5.0.10586.122
+    # CLRVersion                     4.0.30319.42000
+    $dll = Join-Path . "AE.NET.Mail.dll"
+    Add-Type -Path $dll
+
+    $msg = New-Object AE.Net.Mail.MailMessage
+
+    # Fromはプロパティにセット
+    $from = New-Object System.Net.Mail.MailAddress $mail["from"]
+    $msg.From = $from
+    # $msg.From.Add($from) #=> null 値の式ではメソッドを呼び出せません。 
+
+    # ToはAdd()を使う
+    $to = New-Object System.Net.Mail.MailAddress $mail["to"]
+    $msg.To.Add($to)
+    # $msg.To = $to #=> "To" は ReadOnly のプロパティです。
+
+    # ReplyToがないと正常に送信されたにも関わらず、Bounceメールが届く
+    # http://stackoverflow.com/questions/28122074/gmail-api-emails-bouncing
+    # ReplyToもAddを使う
+    $msg.ReplyTo.Add($from)
+    # $msg.ReplyTo = $from  #=> "ReplyTo" は ReadOnly のプロパティです。 
+
+    $msg.Subject = "gmail api subject"
+    $msg.Body = "body: ハロー Gmail API!"
+
+    # https://msdn.microsoft.com/ja-jp/library/system.io.stringwriter.aspx
+    $sw = New-Object System.IO.StringWriter
+    $msg.Save($sw)
+    $raw = ConvertTo-Base64Url $sw.ToString()
+
+    # メールボディはJSONにする
+    $body = @{ "raw" = $raw; } | ConvertTo-Json
+
+    # ユーザIDはURL encodeした値、もしくは`me`で認証したユーザとなる
+    # https://developers.google.com/gmail/api/v1/reference/users/messages/send#http-request
+    # $user_id = [System.Net.WebUtility]::UrlEncode($mail["from"])
+    $user_id = "me"
+    $uri = "https://www.googleapis.com/gmail/v1/users/$($user_id)/messages/send?access_token=$($credential.access_token)"
+
+    try {
+        $result = Invoke-RestMethod $uri -Method POST -ErrorAction Stop -Body $body -ContentType "application/json"
+    }
+    catch [System.Exception] {
+        Write-Host $Error
+        return
+    }
+    Write-Host $result
+}
+
+
+# エントリポイント
+Send-Gmail
---
+++ gmail_api/google_credential.psm1
@@ -0,0 +1,122 @@
+﻿set CREDENTIAL_FILE (Join-Path . "credential.json")
+set SECRET_FILE (Join-Path . "client_id.json")
+set DATE_FORMAT "yyyy/MM/dd HH:mm:ss"
+set GMAIL_SCOPE "https://www.googleapis.com/auth/gmail.send"
+
+
+
+function Save-GoogleCredential($credential){
+    # 有効期限の確認をするため、作成日をセットしておく(上書きする場合に備え、-Forceとする)
+    $credential | Add-Member created_at (Get-Date).ToString($DATE_FORMAT) -Force
+
+    # 扱いやすいJSON形式でトークンを保存しておく
+    $credential_file = Join-Path . $CREDENTIAL_FILE
+    $credential | ConvertTo-Json | Out-File $CREDENTIAL_FILE -Encoding utf8
+}
+
+function Get-GoogleCredential(){
+
+    # https://gallery.technet.microsoft.com/scriptcenter/9e5733bc-8d72-41d7-a6c2-2936cb2cb7fc
+    if (-not(Test-Path $SECRET_FILE)) {
+        Write-Host "Not found client_id.json file"
+        return $null
+    }
+
+    # client_id.jsonファイルから、認証情報を取得する
+    $json = Get-Content $SECRET_FILE -Encoding UTF8 -Raw | ConvertFrom-Json
+    $auth = $json.installed
+
+    
+    if (Test-Path $CREDENTIAL_FILE) {
+        $current_credential = Get-Content $CREDENTIAL_FILE -Encoding UTF8 -Raw | ConvertFrom-Json
+        if (-not ($current_credential.access_token -and $current_credential.token_type -and $current_credential.expires_in `
+                  -and $current_credential.refresh_token -and $current_credential.created_at))
+        {
+            # アクセストークンファイルが正しい形式になっていない場合、処理できない
+            Write-Host "No credential file: $($CREDENTIAL_FILE)"
+            return $null
+        }
+
+        $elapsed_seconds = ((Get-Date) - [DateTime]::ParseExact($current_credential.created_at, $DATE_FORMAT, $null)).TotalSeconds
+        if ($elapsed_seconds -lt $current_credential.expires_in ) {
+            # expireしてない場合は、アクセストークンを再利用
+            Write-Host "Reuse access token..."
+            return $current_credential
+        }
+        else{
+            # expireしてたら、リフレッシュする
+            Write-Host "Refresh access token..."
+
+            $refresh_body = @{
+                "refresh_token" = $current_credential.refresh_token;
+                "client_id" = $auth.client_id;
+                "client_secret" = $auth.client_secret;
+                "grant_type" = "refresh_token";
+            }
+
+            try {
+                $refreshed_credential = Invoke-RestMethod -Method Post -Uri $auth.token_uri -Body $refresh_body
+            }
+            catch [System.Exception] {
+                Write-Host $Error
+                return $null
+            }
+            
+            Save-GoogleCredential $refreshed_credential
+            return $refreshed_credential
+        }
+    }
+
+    # 上記以外の場合は、初回実行と考える
+    Write-Host "New access token..."
+
+    $gmail_scope = "https://www.googleapis.com/auth/gmail.send"
+
+    $auth_url = "$($auth.auth_uri)?scope=$($GMAIL_SCOPE)"
+    $auth_url += "&redirect_uri=$($auth.redirect_uris[0])"
+    $auth_url += "&client_id=$($auth.client_id)"
+    $auth_url += "&response_type=code&approval_prompt=force&access_type=offline"
+
+    # IEを起動して、タイトルにcodeが入ったときにcodeを取得して閉じる
+    # http://qiita.com/fujimohige/items/5aafe5604a943f74f6f0
+    $ie = New-Object -ComObject InternetExplorer.Application
+    $ie.Navigate($auth_url)
+    $ie.Visible = $true 
+
+    $code = ""
+    while($true){
+        # http://canal22.org/advance/ie/ie-documen/
+        # http://d.hatena.ne.jp/ritou/20110414/1302711014
+        $title = $ie.Document.title
+        if (($title) -and ($title.Contains("Success"))) {
+            $code = $title.Replace("Success code=", "")
+            break
+        }
+        Start-Sleep -s 1
+    }
+
+    # https://technet.microsoft.com/en-us/library/ff730962.aspx
+    $ie.Quit()
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($ie)
+    Remove-Variable ie
+
+    try {
+        $new_body = @{
+            "client_id" = $auth.client_id;
+            "client_secret" = $auth.client_secret;
+            "redirect_uri" = $auth.redirect_uris[0];
+            "grant_type" = "authorization_code";
+            "code" = $code;
+        }
+        # https://technet.microsoft.com/ja-jp/library/hh849971.aspx
+        $new_credential = Invoke-RestMethod -Method Post -Uri $auth.token_uri -Body $new_body
+    }
+    catch [System.Exception] {
+        Write-Host $Error
+    }
+    
+    Save-GoogleCredential $new_credential
+    return $new_credential
+}
+
+Export-ModuleMember –Function Get-GoogleCredential
